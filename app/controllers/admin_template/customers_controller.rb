92cbeca61901713bd0fcc878aaa80cab62ebc361
Adoniran France <adoniranfranca@gmail.com>
2023-06-15 22:02:05 -0300

    controllers config to customers

--- app/controllers/admin_template/customers_controller.rb
+++ app/controllers/admin_template/customers_controller.rb
@@ -1,7 +1,6 @@
 class AdminTemplate::CustomersController < AdminTemplateController
-  def index
-    @customers = Customer.all
-  end
+  before_action :set_customer, only: [:index, :edit, :update, :show, :destroy]
+  def index;end
 
   def new
     @customer = current_admin.customers.build
@@ -17,7 +16,35 @@ class AdminTemplate::CustomersController < AdminTemplateController
     end
   end
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to admin_template_customer_url, notice: 'Cliente atualizado'
+    else
+      render :edit
+      flash[:error] = 'Existem campos inválidos'
+    end
+  end
+
+  def show;end
+
+  def destroy
+    if @customer.destroy
+      redirect_to admin_template_customers_path, notice: "Cliente #{@customer.name} excluído"
+    end
+  end
+
   private
+
+  def set_customer
+    if action_name != 'index'
+      @customer = Customer.find(params[:id])
+    end
+    @customers = current_admin.customers
+  end
 
   def customer_params
     params.require(:customer).permit(:name,
