9b29b0438e1f7812490dd44a915e6355fae39b71
Adoniran France <adoniranfranca@gmail.com>
2023-06-22 17:50:04 -0300

    status in sales

---
+++ app/helpers/sales_helper.rb
@@ -0,0 +1,5 @@
+module SalesHelper
+  def available_payment_methods
+    ['Pix', 'Dinheiro' ,'Cartão de Crédito', 'Transferência Bancária']
+  end
+end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,4 +2,14 @@ class Sale < ApplicationRecord
   belongs_to :product
   belongs_to :admin
   belongs_to :customer
+
+  STATUS_OPTIONS = {
+    pending: 'Pendente',
+    in_progress: 'Em andamento',
+    completed: 'Concluída',
+    canceled: 'Cancelada',
+    refunded: 'Reembolsada',
+    awaiting_payment: 'Aguardando pagamento',
+    in_review: 'Em análise'
+  }.freeze
 end
