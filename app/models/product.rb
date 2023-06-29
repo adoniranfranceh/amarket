22c2b181b2426c598760f0452c7cda7239a2da71
Adoniran France <adoniranfranca@gmail.com>
2023-06-29 16:27:18 -0300

    photo

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :admin
   has_and_belongs_to_many :sales
+  has_many :variations
   has_one_attached :image
 end
--- app/models/variation.rb
+++ app/models/variation.rb
@@ -1,3 +1,4 @@
 class Variation < ApplicationRecord
   belongs_to :product
+  has_one_attached :photo
 end
