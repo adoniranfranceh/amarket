fec3c8883334a96084896479155802600f582924
Adoniran France <adoniranfranca@gmail.com>
2023-07-04 09:44:41 -0300

    adding subgroup option in product form

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -14,4 +14,12 @@ module ProductsHelper
       image_tag('no_image.png',  class: 'image-t no_image', value: product.id)
     end
   end
+
+  def is_present?(model, text , form, attribute,class_name)
+    if action_name == 'show'
+      model.present? ? text.titleize : "Sem #{text}"
+    else
+      link_to_add_association "Adicionar #{text}", form, attribute, class: class_name
+   end
+  end
 end
