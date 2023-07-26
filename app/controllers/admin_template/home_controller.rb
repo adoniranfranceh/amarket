2bf6060f93e2aef011bdb7014d75dc09fd228b56
Adoniran France <adoniranfranca@gmail.com>
2023-07-26 12:53:17 -0300

    starting dashboard

--- app/controllers/admin_template/home_controller.rb
+++ app/controllers/admin_template/home_controller.rb
@@ -1,3 +1,12 @@
 class AdminTemplate::HomeController < AdminTemplateController
-  def index;end
+  def index
+
+    @choose_date = Date.new(2023, 7, 25)
+    @total = 0
+    @sales = current_admin.sales.where("DATE(created_at) = ?", @choose_date).each { |sale|
+      @total += sale.total_price
+    }
+
+    @secondary_products = current_admin.secondaryproducts
+  end
 end
